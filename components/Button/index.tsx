0eb217eaf868317a2002d8eeff2e551ab06e7650
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-01-30 21:07:38 +0800

    Next Link

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Link from "next/link";
 interface ButtonProps {
     children: JSX.Element | string;
     href: string;
@@ -6,14 +8,15 @@ interface ButtonProps {
 
 const Button = ({ children, href, targetBlank = false }: ButtonProps) => {
     return (
-        <a
-            className="py-2 px-4 bg-feli rounded-md font-bold uppercase shadow-none hover:shadow-lg text-gray-900 hover:text-white transition"
-            href={href}
-            target={targetBlank ? "_blank" : "_self"}
-            rel="noreferrer"
-        >
-            {children}
-        </a>
+        <Link href={href}>
+            <a
+                className="py-2 px-4 bg-feli rounded-md font-bold uppercase shadow-none hover:shadow-lg text-gray-900 hover:text-white transition"
+                target={targetBlank ? "_blank" : "_self"}
+                rel="noreferrer"
+            >
+                {children}
+            </a>
+        </Link>
     );
 };
 
