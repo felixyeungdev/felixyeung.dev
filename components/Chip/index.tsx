e593187554e8e330e40d380098270606deb4b0be
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-01-30 15:33:49 +0800

    Growing chips

--- components/Chip/index.tsx
+++ components/Chip/index.tsx
@@ -4,7 +4,7 @@ interface ChipProps {
 
 const Chip = ({ label }: ChipProps) => {
     return (
-        <div className="m-2 py-2 px-4 flex items-center justify-center bg-white dark:bg-black rounded-full">
+        <div className="m-2 py-2 px-4 flex flex-grow lg:flex-grow-0 items-center justify-center bg-white dark:bg-black rounded-full">
             {label}
         </div>
     );
