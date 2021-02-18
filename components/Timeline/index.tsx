63005c721135fd7759eb2cbf4970e4ff3a9b50ae
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-02-18 10:54:48 +0800

    Changed UI

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -18,15 +18,26 @@ const Timeline = ({ data, reverse = false }: TimelineProps) => {
     const timelineData: TimelineData[] = arraySort(data, "date", { reverse });
     var currentYear: string;
 
+    const allYears = [
+        ...new Set(
+            timelineData.map((item) => moment(item.date).format("YYYY"))
+        ),
+    ];
+
     return (
         <>
             {timelineData.map((item) => {
                 const itemYear = moment(item.date).format("YYYY");
                 const firstOfKind = itemYear !== currentYear;
                 if (firstOfKind) currentYear = itemYear;
+                const latest =
+                    allYears.indexOf(itemYear) ===
+                    (reverse ? 0 : allYears.length - 1);
                 return (
                     <React.Fragment key={item.name}>
-                        {firstOfKind && <TimelineLabel label={itemYear} />}
+                        {firstOfKind && (
+                            <TimelineLabel label={itemYear} latest={latest} />
+                        )}
                         <TimelineItem data={item} />
                     </React.Fragment>
                 );
@@ -37,12 +48,19 @@ const Timeline = ({ data, reverse = false }: TimelineProps) => {
 
 interface TimelineLabelProps {
     label: string;
+    latest?: boolean;
 }
 
-const TimelineLabel = ({ label }: TimelineLabelProps) => {
+const TimelineLabel = ({ label, latest = false }: TimelineLabelProps) => {
     return (
-        <div className="border-l-4 ml-4 pl-6 pt-3 border-feli flex items-center relative">
-            <div className="h-6 w-6 bg-feli rounded-full absolute -left-3.5 shadow-md"></div>
+        <div className="border-l-4 ml-4 pl-6 pt-3 border-feli-light flex items-center relative">
+            <div
+                className={`h-3 w-3 bg-feli rounded-full absolute -left-2 shadow-md ${
+                    latest
+                        ? "ring-offset-4 ring-offset-feli-lightest ring-4 ring-feli"
+                        : "ring-4 ring-feli-lightest"
+                }`}
+            ></div>
             <h2 className="text-2xl font-semibold">{label}</h2>
         </div>
     );
@@ -54,7 +72,7 @@ interface TimelineItemProps {
 
 const TimelineItem = ({ data }: TimelineItemProps) => {
     return (
-        <div className="border-l-4 ml-4 pl-12 border-feli">
+        <div className="border-l-4 ml-4 pl-12 border-feli-light">
             {data.url ? (
                 <a
                     className="text-lg"
