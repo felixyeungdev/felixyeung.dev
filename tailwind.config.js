b8e699969573284084495831d8ea07bf6b967ffc
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-08-12 07:03:51 +0800

    uses just-in-time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: "media", // or 'media' or 'class'
+    mode: "jit",
     theme: {
         extend: {
             colors: {
